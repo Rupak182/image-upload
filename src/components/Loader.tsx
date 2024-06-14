cf9f6af79743b87d7feaa7aa800af17cd765ee9f
Rupak182 <166648013+Rupak182@users.noreply.github.com>
2024-06-14 18:01:03 +0530

    Update Loader.tsx
    Found a small grammar mistake  while checking the functionality.

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,7 +2,7 @@ export const Loader = () => {
   return (
     <div className="flex flex-col gap-4 items-center justify-center bg-cc-white dark:bg-cc-midnight-blue mx-4 rounded-lg px-20 py-8 w-full md:w-auto shadow-lg">
       <p className="font-medium text-center dark:text-white">
-        Uploading, <span className="font-normal">plase wait..</span>
+        Uploading, <span className="font-normal">please wait..</span>
       </p>
       <div className="text-white rounded-2 relative w-full md:w-[320px] h-[6px] bg-gray-200 overflow-hidden rounded-lg">
         <div className="absolute left-0 top-0 h-full w-1/3 bg-blue-500 animate-loading-bar rounded-lg"></div>
